8b4c93a9f4f4f87a3ed5f3cb23db3ebc29aa06c6
misraishan <59212093+misraishan@users.noreply.github.com>
2025-08-23 15:50:17 -0400

    Remove swiftdata example temporarily

--- GetClipped/App/GetClippedApp.swift
+++ GetClipped/App/GetClippedApp.swift
@@ -12,18 +12,18 @@ import SwiftData
 struct GetClippedApp: App {
     @StateObject private var clipboardMonitor = ClipboardMonitor()
 
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+//    var sharedModelContainer: ModelContainer = {
+//        let schema = Schema([
+//            Item.self,
+//        ])
+//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+//
+//        do {
+//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+//        } catch {
+//            fatalError("Could not create ModelContainer: \(error)")
+//        }
+//    }()
     
     var body: some Scene {
         WindowGroup {
@@ -31,6 +31,6 @@ struct GetClippedApp: App {
                 .environmentObject(clipboardMonitor)
         }
         .windowResizability(.contentSize)
-        .modelContainer(sharedModelContainer)
+//        .modelContainer(sharedModelContainer)
     }
 }
